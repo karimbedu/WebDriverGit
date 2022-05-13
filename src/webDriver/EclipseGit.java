c897389ba3086e6beb46879ccddfd991646ef322
karimbedu <kpurushotham.777@gmail.com>
2022-05-13 09:58:40 +0530

    Through eclipse sending

---
+++ src/webDriver/EclipseGit.java
@@ -0,0 +1,14 @@
+package webDriver;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class EclipseGit {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
